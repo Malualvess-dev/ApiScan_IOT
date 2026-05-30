8901ac194232e4b62ae20d4353280ee81b744fdf
Malu <maluaquino1007@gmail.com>
2026-05-30 17:05:05 -0300

    Adicionando projeto

---
+++ src/main/java/com/agroscan/apiscan/ApiscanApplication.java
@@ -0,0 +1,13 @@
+package com.agroscan.apiscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroscan/apiscan/ApiscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroscan.apiscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
